0096332b45bdb50f298e9aa9fc92cc1b3a7e9423
unknown <lazioteamkochi@gmail.com>
2024-10-22 13:01:50 -0700

    nav finished

--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -28,7 +28,7 @@ const NavBar = () => {
 
   return (
     <nav className={styles.nav}>
-      <div style={{position:"fixed"}} className={styles.menuIcon} onClick={handleMenuToggle}>
+      <div  className={styles.menuIcon} onClick={handleMenuToggle}>
         {open ? <RiCloseLargeFill /> : <GrMenu />}
       </div>
 
